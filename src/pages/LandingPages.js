dbe4dc902470bd0e2a2d498d92c68adf9629bed3
rashadhrrs <rashadhrrs@users.noreply.github.com>
2020-08-26 17:30:39 +0700

    Add Testimonial Section

--- src/pages/LandingPages.js
+++ src/pages/LandingPages.js
@@ -5,6 +5,7 @@ import landingPage from "json/landingPage.json";
 import Hero from "parts/Hero";
 import MostPicked from "parts/MostPicked";
 import Categories from "parts/Categories"
+import Testimony from "parts/Testimony";
 
 export default class LandingPages extends Component {
   constructor(props) {
@@ -21,6 +22,7 @@ export default class LandingPages extends Component {
           data={landingPage.mostPicked}
         />
         <Categories data={landingPage.categories}/>
+        <Testimony data={landingPage.testimonial}/>
       </>
     );
   }
